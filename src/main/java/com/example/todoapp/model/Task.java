07b1fb0a0a9ab9580407085492e10ce8c930f3fd
ryc-o <rytel.mateusz@wp.pl>
2022-05-31 22:26:33 +0200

    Add sample project

---
+++ src/main/java/com/example/todoapp/model/Task.java
@@ -0,0 +1,41 @@
+package com.example.todoapp.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "task")
+public class Task{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int  id;
+    @NotBlank(message = "Task's description most be not null and not empty")
+    private String description;
+    private boolean done;
+    public Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void setDone(boolean done) {
+        this.done = done;
+    }
+}
