0f04fc0e6a86d7b1d64a8aabca3b90f93b534dff
ryc-o <rytel.mateusz@wp.pl>
2022-06-06 20:25:15 +0200

    Added constraints for deleting tasks

--- src/main/java/com/example/todoapp/model/TaskRepository.java
+++ src/main/java/com/example/todoapp/model/TaskRepository.java
@@ -2,8 +2,15 @@ package com.example.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
 
+    @Override
+    @RestResource(exported = false)
+    void delete(Task entity);
 }
