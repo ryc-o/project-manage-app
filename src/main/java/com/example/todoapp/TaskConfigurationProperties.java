ee3dd8a3ee81bac348fb4104c3e549a0bb46418c
ryc-o <rytel.mateusz@wp.pl>
2022-06-10 01:21:22 +0200

    Changed yaml files

--- src/main/java/com/example/todoapp/TaskConfigurationProperties.java
+++ src/main/java/com/example/todoapp/TaskConfigurationProperties.java
@@ -1,2 +1,30 @@
-package com.example.todoapp;public class TaskConfigurationProperties {
+package com.example.todoapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("task")
+public class TaskConfigurationProperties {
+    private Template tamplate;
+
+    public Template getTamplate() {
+        return tamplate;
+    }
+
+    public void setTamplate(Template tamplate) {
+        this.tamplate = tamplate;
+    }
+
+    public static class Template{
+        private boolean allowMultipleTasks;
+
+        public boolean isAllowMultipleTasks() {
+            return allowMultipleTasks;
+        }
+
+        public void setAllowMultipleTasks(boolean allowMultipleTasks) {
+            this.allowMultipleTasks = allowMultipleTasks;
+        }
+    }
 }
