3ba7e9d8e07d37c598659483cf7e8fec40d11863
ryc-o <rytel.mateusz@wp.pl>
2022-06-21 00:13:55 +0200

    Added project service

---
+++ src/main/java/com/example/todoapp/logic/ProjectService.java
@@ -0,0 +1,51 @@
+package com.example.todoapp.logic;
+
+import com.example.todoapp.TaskConfigurationProperties;
+import com.example.todoapp.model.*;
+import com.example.todoapp.model.projection.GroupReadModel;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProjectService {
+    private ProjectRepository repository;
+    private TaskGroupRepository taskGroupRepository;
+    private TaskConfigurationProperties config;
+
+    public ProjectService(ProjectRepository repository, TaskGroupRepository taskGroupRepository, TaskConfigurationProperties config) {
+        this.repository = repository;
+        this.taskGroupRepository = taskGroupRepository;
+        this.config = config;
+    }
+
+    public List<Project> readAll() {
+        return repository.findAll();
+    }
+
+    public Project save(Project toSave){
+        return repository.save(toSave);
+    }
+
+    public GroupReadModel createGroup(LocalDateTime deadline, int projectId) {
+        if (config.getTamplate().isAllowMultipleTasks() && taskGroupRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
+            throw new IllegalStateException("Only one undone group from project is allowed");
+        }
+        TaskGroup result = repository.findById(projectId)
+                .map(project -> {
+                    var targetGroup = new TaskGroup();
+                    targetGroup.setDescription(project.getDescription());
+                    targetGroup.setTasks(
+                            project.getSteps().stream()
+                                    .map(projectStep -> new Task(
+                                            projectStep.getDescription(),
+                                            deadline.plusDays(projectStep.getDaysToDeadline()))
+                                    ).collect(Collectors.toSet())
+                    );
+                    return targetGroup;
+                }).orElseThrow(() -> new IllegalArgumentException("PRoject with given id not found"));
+        return new GroupReadModel(result);
+    }
+}
